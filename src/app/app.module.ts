d189f563056f0c6c66d7ea90a3f9ae00ac797970
Samuel-Clarke123 <clarkepearl44@gmail.com>
2021-03-16 13:51:28 +1100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MdbModule } from 'mdb-angular-ui-kit';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MdbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
