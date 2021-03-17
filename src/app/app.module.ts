bcf31f4e3e93c52464cf1fc2751157268aacb655
Samuel-Clarke123 <clarkepearl44@gmail.com>
2021-03-17 12:39:02 +1100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { MdbModule } from 'mdb-angular-ui-kit';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContentComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
